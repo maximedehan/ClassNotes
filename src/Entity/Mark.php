40fdce3082d814d481bd4f5b292d597759ad541c
maximedehan <maximedehan@aol.com>
2020-08-14 14:43:15 +0200

    First commit

---
+++ src/Entity/Mark.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Mark
+ *
+ * @ORM\Table(name="mark", indexes={@ORM\Index(name="id_student", columns={"id_student"})})
+ * @ORM\Entity
+ */
+class Mark
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="value", type="float", nullable=false)
+     */
+    private $value;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lesson", type="string", length=255, nullable=false)
+     */
+    private $lesson;
+
+    /**
+     * @var \Student
+     *
+     * @ORM\ManyToOne(targetEntity="Student")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_student", referencedColumnName="id")
+     * })
+     */
+    private $idStudent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getLesson(): ?string
+    {
+        return $this->lesson;
+    }
+
+    public function setLesson(string $lesson): self
+    {
+        $this->lesson = $lesson;
+
+        return $this;
+    }
+
+    public function getIdStudent(): ?Student
+    {
+        return $this->idStudent;
+    }
+
+    public function setIdStudent(?Student $idStudent): self
+    {
+        $this->idStudent = $idStudent;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/Student.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Student
+ *
+ * @ORM\Table(name="student")
+ * @ORM\Entity
+ */
+class Student
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255, nullable=false)
+     */
+    private $firstname;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="birthday", type="string", length=255, nullable=true)
+     */
+    private $birthday;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getBirthday(): ?string
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(?string $birthday): self
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+
+}
